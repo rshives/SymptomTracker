dff3f805710b90ce586bcb347ee26949dafbe471
rshives <shivesster@gmail.com>
2014-08-12 13:09:54 +0000

    Added testing frameworks

--- gruntfile.js
+++ gruntfile.js
@@ -5,29 +5,66 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     
     jshint: {
-      all: ['app/scripts/app.js', 'test/angular/controllerSpec.js']
+      scripts: ['app/scripts/app.js'],
+      test: ['test/unit/spec/controllersSpec.js'],
+      all: ['app/scripts/app.js', 'test/unit/spec/controllersSpec.js']
+    },
+    copy: {
+      scripts: {
+        files: [
+            { expand: true, src: ['app/scripts/*'], dest: 'build/scripts/', filter: 'isFile' },
+        ],
+      },
+      styles: {
+        files: [
+          { expand: true, src: ['app/style/*'], dest: 'build/style/', filter: 'isFile' },  
+        ],
+      },
+      view: {
+        files: [
+          { src: 'app/index.html', dest: 'build/app.html' },  
+        ],
+      },
     },
     connect: {
       server: {
         options: {
           port: 8000,
-          keepalive: true,
-          /*livereload: true*/
-        }
-      }
+        },
+      },
     },
     watch: {
       scripts: {
-        files: ['app/index.html', 'app/scripts/app.js', 'test/angular/controllerSpec.js'],
-        tasks: ['jshint'],
+        files: ['app/scripts/*',],
+        tasks: ['jshint:scripts', 'copy:scripts'],
         options: {
           spawn: false,
-          
-        }
-      }
-    }
+        },
+      },
+      styles: {
+        files: ['app/style/*'],
+        tasks: ['copy:styles'],
+        options: {
+          spawn: false,
+        },
+      },
+      view: {
+        files: ['app/index.html'],
+        tasks: ['copy:view'],
+        options: {
+          spawn: false,
+        },
+      },
+      test: {
+        files: ['test/unit/spec/*'],
+        tasks: ['jshint:test'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
     
   });
   
-  grunt.registerTask('default', ['jshint', 'connect','watch']);
+  grunt.registerTask('default', ['jshint:all', 'connect', 'watch',]);
 };
