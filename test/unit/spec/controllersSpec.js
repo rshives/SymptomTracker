e863229931d24594dc1abc5728a90bc1aab29eb4
rshives <shivesster@gmail.com>
2014-08-12 13:05:46 +0000

    added testing frameworks

--- test/unit/spec/controllersSpec.js
+++ test/unit/spec/controllersSpec.js
@@ -1,8 +1,17 @@
+/** test/unit/spec/controllersSpec.js */
+
 describe('linkCtrl', function() {
+  var scope;
+  var testCtrl;
+  
+  
+  beforeEach(module('symptomTrackerApp'));
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.new();
+    testCtrl = $controllr('linkCtrl', {$scope:scope});
+  }));
+  
   it('should create a links model and process 8 links', function() {
-    var scope = {},
-    linkCtrl = new linkController(scope);
-    
     expect(scope.links.length).tobe(8);
   });
 });
